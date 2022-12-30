743cdc4848d4a5938f637d5e938b23020b794400
Ricky Jenkins <demiwire@gmail.com>
2022-12-30 23:00:58 +0000

    updated button component to handle button rounding style

--- src/lib/ui-library/Button/JButton.svelte
+++ src/lib/ui-library/Button/JButton.svelte
@@ -1,7 +1,32 @@
+<script lang="ts">
+        type JButtonStyles = 'circle' | 'square' | 'rounded';
+
+        let clazz;
+        export { clazz as class }
+
+        export let type: JButtonStyles = 'rounded';
+
+        const buttonStyle = () => {
+                switch (type) {
+                        case "circle":
+                                return "rounded-full";
+                        case "rounded":
+                                return "rounded-lg";
+                        case "square":
+                                return "rounded-none";
+                        default:
+                                return "";
+                }
+        }
+
+
+</script>
 <button
         on:click
         type="button"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-    <slot />
+        class="text-center inline-flex items-center bg-blue-700 dark:bg-blue-700 text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium text-sm px-5 py-2.5 mr-2 mb-2 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 {clazz} {buttonStyle()}">
+        <slot name="prefix"></slot>
+        <slot />
+        <slot name="suffix"></slot>
 </button>
 
