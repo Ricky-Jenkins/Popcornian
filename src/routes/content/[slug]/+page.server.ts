632c97aa895b7097e258dc98bb4312b6b8fdef85
Ricky Jenkins <demiwire@gmail.com>
2022-12-30 17:46:09 +0000

    updated path to Film type and updated app.d.ts to include a declared module for font awesome.

--- src/routes/content/[slug]/+page.server.ts
+++ src/routes/content/[slug]/+page.server.ts
@@ -1,5 +1,5 @@
 import { films } from "$lib/server/repository/media";
-import type {Film} from "$lib/server/types/media/Film";
+import type {Film} from "$lib/types/media/Film";
 
 /** @type {import('./$types').PageServerLoad} */
 export function load({ params }: { params: { slug: number }}): { content: Film | undefined } {
